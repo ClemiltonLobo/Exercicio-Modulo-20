63c413d89c52a93ea2c951e90dc65a58e0a5ae76
ClemiltonLobo <clemilton.lobo@gmail.com>
2023-05-23 18:52:14 -0300

    Fazendo umas alterações de Adaptive Performances, e finalizando Menu

--- Assets/Scripts/Utils/LoadScene.cs
+++ Assets/Scripts/Utils/LoadScene.cs
@@ -5,13 +5,55 @@ using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour
 {
+    public Animator animator;
+    public AudioSource AudioSource;
+
     public void Load(int i)
     {
-        SceneManager.LoadScene(i);
+        //Aivar o som "SFX"
+        AudioSource.Play();
+
+        // Ativar a animação "PopAnimPlay" antes de carregar a nova cena
+        animator.SetTrigger("PopAnimPlay");
+
+        // Aguardar a duração da animação (ajuste o tempo de acordo com a animação)
+        float animDuration = GetAnimationDuration("PopAnimPlay");
+        StartCoroutine(LoadSceneAfterDelay(i, animDuration));
     }
 
     public void Load(string s)
     {
+        //Aivar o som "SFX"
+        AudioSource.Play();
+
+        // Ativar a animação "PopAnimPlay" antes de carregar a nova cena
+        animator.SetTrigger("PopAnimPlay");
+
+        // Aguardar a duração da animação (ajuste o tempo de acordo com a animação)
+        float animDuration = GetAnimationDuration("PopAnimPlay");
+        StartCoroutine(LoadSceneAfterDelay(s, animDuration));
+    }
+
+    IEnumerator LoadSceneAfterDelay(int i, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        // Carregar a nova cena após a animação
+        SceneManager.LoadScene(i);
+    }
+
+    IEnumerator LoadSceneAfterDelay(string s, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        // Carregar a nova cena após a animação
         SceneManager.LoadScene(s);
+    }
+
+    float GetAnimationDuration(string animationName)
+    {
+        AnimatorClipInfo[] clipInfo = animator.GetCurrentAnimatorClipInfo(0);
+        AnimationClip animClip = clipInfo[0].clip;
+        return animClip.length;
     }
 }
