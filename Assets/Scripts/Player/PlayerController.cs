cfb1108fda3ac49f27c7a00f5676f936b1d4ceab
ClemiltonLobo <clemilton.lobo@gmail.com>
2023-06-05 12:48:22 -0300

    Adicionando, tag, para definir colisão com obstaculo, e alterando o script de movimentação  do player

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,58 +1,69 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     //publics
-    //[Header("Lerp")]
-    //public Transform target;
-    //public float lerpSpeed = 1f;
+    [Header("Lerp")]
+    public Transform target;
+    public float lerpSpeed = 1f;
     public float speed = 1f;
-    //public string tagToCheckEnemy = "Enemy";
-    //public string tagToCheckEndLine = "EndLine";
-    //public GameObject endScreen;
-
-
-    //privates
-    //private Vector3 _pos;
-    //private bool _canRun;
+    public string tagToCheckEnemy = "Enemy";
+    public string tagToCheckEndLine = "EndLine";   
 
     
+
+    //privates
+    private Vector3 _pos;
+    private bool _canRun;
+
+    private void Start()
+    {
+        _canRun = true;
+    }
+
     void Update()
     {
-        //if(!_canRun) return;
-        //_pos = target.position;
-        //_pos.y = transform.position.y;
-        //_pos.z = transform.position.z;
+        if(!_canRun) return;
+        _pos = target.position;
+        _pos.y = transform.position.y;
+        _pos.z = transform.position.z;
 
-        //transform.position = Vector3.Lerp(transform.position, _pos, lerpSpeed * Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, _pos, lerpSpeed * Time.deltaTime);
         transform.Translate(transform.forward * speed * Time.deltaTime);
     }
 
-    /*private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
         if(collision.transform.tag == tagToCheckEnemy)
         {
-            EndGame();
+            LoadLoserScene();
         }
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.tag == tagToCheckEndLine)
         {
-            EndGame();
+            LoadWinnerScene();
         }
     }
 
-    private void EndGame()
+    private void LoadWinnerScene()
+    {
+        SceneManager.LoadScene("WinnerScene");
+    }
+
+    private void LoadLoserScene()
     {
         _canRun = false;
-        endScreen.SetActive(true);
+        SceneManager.LoadScene("LoserScene");
     }
 
     public void StartToRun()
     {
         _canRun=true;
-    }*/
+    }
 }
